c03011dc14c114371e360dbcfe0fa3150f4060b5
rs4502479-coder <rs4502479@gmail.com>
2025-11-27 00:25:59 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
-  ssl: { rejectUnauthorized: false }  // Render requires SSL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
